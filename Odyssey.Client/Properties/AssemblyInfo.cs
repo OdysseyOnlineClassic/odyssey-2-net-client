b028f512a54edaec235599b51867a8b557159b3d
Bryan Bennett <bennett.pgmr@gmail.com>
2018-02-20 00:17:42 -0500

    init

---
+++ Odyssey.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Odyssey.Client")]
+[assembly: AssemblyProduct("Odyssey Online")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyDescription("The Odyssey Online Client")]
+[assembly: AssemblyCompany("OdysseyOnline")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7aa48cf-8168-435f-81fb-3f50fdffe4f7")]
+
+[assembly: AssemblyVersion("0.1.*")]
