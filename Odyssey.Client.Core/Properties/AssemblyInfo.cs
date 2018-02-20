eedb4f6bfe7788e1ea1e6a02b7d254f5239714a7
Bryan Bennett <bennett.pgmr@gmail.com>
2018-02-20 00:34:17 -0500

    Added odyssey.ico.  Added core project.  Implemented common assembly info.  Added Unity IoC nuget reference.

---
+++ Odyssey.Client.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Odyssey.Client.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("551d2ea1-77a2-4524-8da5-3657f518d280")]
+
+[assembly: AssemblyVersion("0.1.*")]
---
+++ Odyssey.Client/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyProduct("The Odyssey Online Game")]
+[assembly: AssemblyCompany("OdysseyOnline")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
